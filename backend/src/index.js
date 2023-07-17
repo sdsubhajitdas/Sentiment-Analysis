16e00175b9c82c059eb706ad24c8af99f048563e
Subhajit Das <sdsubhajitdas98@gmail.com>
2023-07-18 00:42:00 +0530

    Backend cors rule updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const app = express();
 // Hooking up middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 
 // Hooking up routes
 app.use("/auth", authRouter);
