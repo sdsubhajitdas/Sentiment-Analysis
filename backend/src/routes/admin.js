8076dc0330b56a37c265219b10fb77f4368f1601
Subhajit Das <sdsubhajitdas98@gmail.com>
2023-08-15 20:46:37 +0530

    Backend Role and Status toggling routes added

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -14,4 +14,26 @@ router.get("/users", async (req, res) => {
   );
 });
 
+router.post("/toggle-user-status", async (req, res) => {
+  const userId = req.body.userId;
+
+  let user = await User.findById(userId);
+
+  user.status = user.status === "ACTIVE" ? "INACTIVE" : "ACTIVE";
+  await user.save();
+
+  res.status(202).send();
+});
+
+router.post("/toggle-admin-role", async (req, res) => {
+  const userId = req.body.userId;
+
+  let user = await User.findById(userId);
+
+  user.role = user.role === "ADMIN" ? "USER" : "ADMIN";
+  await user.save();
+
+  res.status(202).send();
+});
+
 module.exports = router;
