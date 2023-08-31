5b5fb03bdf3aeb10080e4238cf58b6144940409e
Subhajit Das <sdsubhajitdas98@gmail.com>
2023-08-31 22:37:22 +0530

    Integrated ML code

--- mlBackend/utils/generator.py
+++ mlBackend/utils/generator.py
@@ -1,8 +1,11 @@
 import os
 import random
 import matplotlib
+import numpy as np
+from joblib import load
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 matplotlib.use('agg')
 
@@ -16,6 +19,28 @@ def generate_wordcloud(text, query_id):
   plt.savefig(os.path.join("assets",f'{query_id}.png'), format="png")
 
 
-def generate_model_output(text):
-  sentiment = random.choice([-1, 0, 1])
-  return sentiment
+def generate_model_output(preprocessed_text):
+  modelsPath = os.path.join(os.getcwd(), "models")
+
+  # Load the saved vectorizer
+  vectorizer = load(os.path.join(modelsPath, "vectorizer.joblib"))
+  
+  # Vectorize the preprocessed input string
+  vectorized_text = vectorizer.transform([preprocessed_text])
+  
+  # Load the saved Random Forest model
+  rf_model = load(os.path.join(modelsPath,"logistic_regression.joblib"))
+  
+  # Make the prediction
+  prediction = int(rf_model.predict(vectorized_text)[0])  # Convert to native int
+  
+  # Calculate the confidence of the prediction
+  confidence = np.max(rf_model.predict_proba(vectorized_text))
+  
+  # Create and return JSON
+  result = {
+      "sentiment": prediction,
+      "confidence": confidence.item()  # Convert numpy.float64 to native float
+  }
+
+  return result["sentiment"]
--- mlBackend/utils/preprocessing.py
+++ mlBackend/utils/preprocessing.py
@@ -1,15 +1,56 @@
 import re
+import nltk
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
+from nltk.stem import PorterStemmer, WordNetLemmatizer
 
 
 def preprocess_text(text):
-  text = re.sub('[^a-zA-Z]', ' ', text)
+  # 1. Remove URLs
+  text = re.sub(r'http\S+|www.\S+', '', text)
+  
+  # 2. Remove usernames
+  text = re.sub(r'@\w+', '', text)
+  
+  # 3. Remove email addresses
+  text = re.sub(r'\S+@\S+', '', text)
+  
+  # 4. Remove hashtags
+  text = re.sub(r'#\w+', '', text)
+  
+  # 5. Remove HTML tags
+  text = re.sub(r'<.*?>', '', text)
+  
+  # 6. Remove stock market tickers like $GE
+  text = re.sub(r'\$\w+', '', text)
+  
+  # 7. Remove dates
+  text = re.sub(r'\b\d{1,2}[-/]\d{1,2}[-/]\d{2,4}\b', '', text)
+  
+  # 8. Remove times
+  text = re.sub(r'\b\d{1,2}:\d{1,2}\b', '', text)
+  
+  # 9. Character normalization (e.g., converting multiple occurrences of a character into one)
+  text = re.sub(r'(.)\1+', r'\1\1', text)
+  
+  # 10. Remove punctuation, special characters, and numbers
+  text = re.sub(r'[^a-zA-Z\s]', '', text)
+  
+  # 11. Lower casing
   text = text.lower()
-  words = text.split()
-  stops = set(stopwords.words("english"))
-  meaningful_words = [w for w in words if not w in stops]
-  stemmer= PorterStemmer()
-  stemmed_words = [stemmer.stem(w) for w in meaningful_words]
-  cleaned_text = " ".join(stemmed_words)
-  return cleaned_text
+  
+  # 12. Tokenization
+  text = nltk.word_tokenize(text)
+  
+  # 13. Remove stopwords
+  stop_words = set(stopwords.words('english'))
+  text = [word for word in text if word not in stop_words]
+  
+  # 14. Stemming
+  stemmer = PorterStemmer()
+  text = [stemmer.stem(word) for word in text]
+  
+  # 15. Lemmatization
+  lemmatizer = WordNetLemmatizer()
+  text = [lemmatizer.lemmatize(word) for word in text]
+  
+  return ' '.join(text)
